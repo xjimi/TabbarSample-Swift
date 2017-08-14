872d4e05c9a66346709869977827994ed2d2b461
XJIMI <fn5128@gmail.com>
2017-08-14 18:16:34 +0800

    Initial Commit

---
+++ TabbarSample-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabbarSample-Swift
+//
+//  Created by XJIMI on 2017/8/14.
+//  Copyright © 2017年 Vidol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
